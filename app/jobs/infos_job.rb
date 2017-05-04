8ec021401d4ddf93d520eebc51fb2f81cd94b0e9
yanweizhen <yanweizhen@yimeijian.cn>
2017-05-04 14:52:51 +0800

    fix

--- app/jobs/infos_job.rb
+++ app/jobs/infos_job.rb
@@ -3,6 +3,9 @@ class InfosJob < ActiveJob::Base
 
   def perform(id,stock,industry,plate,report,start_time,end_time)
     res =  CnInfo.find id
+    Rails.logger "######### #{id}"
+    Rails.logger "######### #{res}"
     res.get_result(stock,industry,plate,report,start_time,end_time)
+
   end
 end
