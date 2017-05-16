2ca73402008d11a6964ecf05a66909bdf800aae0
yanweizhen <yanweizhen@yimeijian.cn>
2017-05-16 15:14:18 +0800

    redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://125.208.9.73:6379/12' }
+  config.redis = { url: 'redis://127.0.0.1:6379/12' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://125.208.9.73:6379/12' }
+  config.redis = { url: 'redis://127.0.0.1:6379/12' }
 end
