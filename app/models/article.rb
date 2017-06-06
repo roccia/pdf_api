da925827678e42d4e6a322f8af9190a934813e72
roccia <lzzy.ywz@gmail.com>
2017-06-06 16:38:28 +0800

    fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,13 +7,12 @@ class Article < ApplicationRecord
     io = open(url)
     begin
       reader = PDF::Reader.new(io)
-    rescue PDF::Reader::MalformedPDFError => err
-      self.content = err
-    else
       reader.pages.each do |page|
         content_ary << page.text
       end
       self.content = content_ary
+    rescue PDF::Reader::MalformedPDFError => err
+      self.content = err
     ensure
       self.save
     end
