4100203c7f76ac2f38c53e5a845f750f4c822cd9
roccia <lzzy.ywz@gmail.com>
2017-09-08 15:57:06 +0800

    fix

--- app/jobs/article_job.rb
+++ app/jobs/article_job.rb
@@ -4,7 +4,9 @@ class ArticleJob < ActiveJob::Base
   def perform(id)
      articles = Article.where(:cn_info_id => id)
      articles.each do |a|
+       if a.content.blank?
        a.read_pdf(a.url)
+       end
      end
   end
 end
