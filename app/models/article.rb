703cd60bf1e0c294240d6163e776dad581a344bc
roccia <lzzy.ywz@gmail.com>
2017-09-22 16:50:02 +0800

    fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,12 +10,29 @@ class Article < ApplicationRecord
       reader.pages.each do |page|
         content_ary << page.text
       end
-       self.content = content_ary
-    rescue PDF::Reader::MalformedPDFError => err
-        self.error_info = "文件不可读取#{err}"
+      self.content = content_ary
+    rescue PDF::Reader::MalformedPDFError, PDF::Reader::EncryptedPDFError,
+        PDF::Reader::InvalidObjectError, PDF::Reader::UnsupportedFeatureError => err
+
+      self.error_info = "文件不可读取#{err}"
     end
   ensure
     self.save
   end
 
+  def run
+    Article.where.not(url: nil).where(content: nil).first(100).each do |a|
+      begin
+        a.read_pdf(a.url)
+      rescue PDF::Reader::MalformedPDFError, PDF::Reader::EncryptedPDFError,
+          PDF::Reader::InvalidObjectError, PDF::Reader::UnsupportedFeatureError => err
+        a.error_info = err
+      ensure
+        a.save
+      end
+
+    end
+  end
+
 end
+
